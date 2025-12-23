23b3243a3cf2fa4eefeef3180c33b1ab3777a3c2
Sc1l3nt <113893981+Sc1l3nt@users.noreply.github.com>
2025-12-23 16:10:21 +0700

    last commit

--- clean_data.py
+++ clean_data.py
@@ -29,7 +29,17 @@ df["positive_reactions"] = (
 df["negative_reactions"] = df["num_sads"] + df["num_angrys"]
 
 # Sentiment proxy (NLP-style feature)
-df["negative_ratio"] = df["negative_reactions"] / df["num_reactions"].replace(0, 1)
+df["positive_ratio"] = df["positive_reactions"] / df["num_reactions"]
+
+# Viral
+p_viral=0.12 # tỉ lệ chuyển đổi trung bình của những lời mời đó
+df['viral']=df['num_shares']*p_viral
+
+# Factored Engagement (Tổng hợp tương tác có trọng số)
+k_comment=2
+k_reaction=1
+k_share=3
+df['factored']=k_comment*df['num_comments']+k_reaction*df['num_reactions']+k_share*df['num_shares']
 
 # =========================
 # 3. DAILY AGGREGATION (PAGE LEVEL)
@@ -39,11 +49,12 @@ daily = (
       .agg(
           post_count=("status_id", "count"),
           reactions=("num_reactions", "median"),
-          comments=("num_comments", "median"),
+          positive_reactions=("positive_reactions", "median"),
+          negative_reactions=("negative_reactions", "median"),
+          avg_positive_ratio=("positive_ratio", "median"),
           shares=("num_shares", "median"),
-          avg_negative_ratio=("negative_ratio", "median"),
-          positive_sum=("positive_reactions", "median"),
-          negative_sum=("negative_reactions", "median")
+          viral=("viral","median"),
+          engagement=('factored','median')
       )
       .reset_index()
 )
@@ -52,56 +63,6 @@ daily = (
 # =========================
 # 4. SLIDING WINDOW 1 WEEK (FEATURE TABLE)
 # =========================
-'''
-feature_rows = []
-   feature_rows.append({
-            "page_id": page,
-            "window_start": window["date"].iloc[0],
-            "window_end": window["date"].iloc[-1],
-
-for status_type, group in daily.groupby("status_type"):
-    group = group.sort_values("time_frame").reset_index(drop=True)
-
-    for i in range(len(group) - 6):
-        window = group.iloc[i:i+7]
-
-        feature_rows.append({
-            "status_type": status_type,
-            "window_start": window["time_frame"].iloc[0].start_time,
-            "window_end": window["time_frame"].iloc[-1].end_time,
-
-            # Activity
-            "total_posts_1w": window["post_count"].sum(),
-            "posts_per_day": window["post_count"].mean(),
-
-            # Engagement
-            "total_reactions_1w": window["reactions"].sum(),
-            "total_comments_1w": window["comments"].sum(),
-            "total_shares_1w": window["shares"].sum(),
-            "engagement_per_post": window["reactions"].sum() / window["post_count"].sum(),
-            "engagement_per_post": (
-                window["reactions"].sum() / window["post_count"].sum()
-                if window["post_count"].sum() > 0 else 0
-            ),
-
-            # NLP / Sentiment features
-            "avg_negative_ratio_1w": window["avg_negative_ratio"].mean(),
-            "positive_reactions_1w": window["positive_sum"].sum(),
-            "negative_reactions_1w": window["negative_sum"].sum(),
-
-            # Trend (time-aware)
-            "reaction_trend": window["reactions"].iloc[-1] - window["reactions"].iloc[0]
-        })
-
-# =========================
-# 5. EXPORT FEATURE TABLE
-# =========================
-feature_table = pd.DataFrame(feature_rows)
-
-print("\nFeature table sample:")
-print(feature_table.head())
-
-'''
 df['day_name']=df['status_published'].dt.day_name()
 
 
@@ -110,17 +71,19 @@ week = (
       .agg(
           post_count=("status_id", "count"),
           reactions=("num_reactions", "median"),
-          comments=("num_comments", "median"),
+          positive_reactions=("positive_reactions", "median"),
+          negative_reactions=("negative_reactions", "median"),
+          avg_positive_ratio=("positive_ratio", "median"),
           shares=("num_shares", "median"),
-          avg_negative_ratio=("negative_ratio", "median"),
-          positive_sum=("positive_reactions", "median"),
-          negative_sum=("negative_reactions", "median")
+          viral=("viral","median"),
+          engagement=('factored','median')
       )
       .reset_index()
 )
 
-print("Daily data sample:")
-print(daily)
-print("Week data sample:")
-print(week)
+# print("Daily data sample:")
+# print(daily)
+# print("Week data sample:")
+# print(week)
 
+print(df)
